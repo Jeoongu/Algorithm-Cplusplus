98e44577732f27353b86afe3032d8a91fc954ac0
Jeoongu <ms235280@naver.com>
2024-01-18 22:29:56 +0900

    add : sliding window and stack, queue

--- Algorithm/1253_좋은수.cpp
+++ Algorithm/1253_좋은수.cpp
@@ -12,4 +12,41 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> A(N, 0);
+
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	sort(A.begin(), A.end());
+	int result = 0;
+
+	for (int k = 0; k < N; k++) {
+		long find = A[k];
+		int i = 0;
+		int j = N - 1;
+
+		while (i < j) {
+			if (A[i] + A[j] == find) { // 서로 다른 두 수의 합인지 체크
+				if (i != k && j != k) {
+					result++;
+					break;
+				}
+				else if (i = k) {
+					i++;
+				}
+				else if (j == k) {
+					j--;
+				}
+			}
+			else if (A[i] + A[j] < find) {
+				i++;
+			}
+			else {
+				j--;
+			}
+		}
+	}
+	cout << result << "\n";
+
+	
 }
