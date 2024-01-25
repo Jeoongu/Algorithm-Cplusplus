e83476e01667051c3146dc5de527cb68faefad8d
Jeoongu <ms235280@naver.com>
2024-01-25 19:41:36 +0900

    23 dfs

---
+++ Algorithm/1427_선택정렬_내림차순으로자릿수정렬.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string str;
+	cin >> str;
+
+	vector<int> A(str.length(), 0);
+
+	for (int i = 0; i < str.size(); i++) {
+		A[i] = stoi(str.substr(i, 1));
+	}
+
+	for (int i = 0; i < str.length(); i++) {
+		int max = i;
+		for (int j = i + 1; j < str.length(); j++) {
+			if (A[j] > A[max]) {
+				max = j;
+			}
+		}
+		if (A[i] < A[max]) {
+			int temp = A[i];
+			A[i] = A[max];
+			A[max] = temp;
+		}
+	}
+	for (int i = 0; i < A.size(); i++) {
+		cout << A[i];
+	}
+}
