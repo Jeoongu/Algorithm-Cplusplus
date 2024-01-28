0ed8ee04a5e8e55bed1a3d7e87599701ae8f5409
Jeoongu <ms235280@naver.com>
2024-01-28 23:21:26 +0900

    fix : boj 11399

--- Algorithm/11399_ ATM.cpp
+++ Algorithm/11399_ ATM.cpp
@@ -12,5 +12,37 @@ int main() {
 	vector<int> A(N, 0);
 	vector<int> S(N, 0);
 
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
 
+	for (int i = 0; i < N; i++) {
+		int insert_point = i;
+		int insert_value = A[i];
+		for (int j = i - 1; j >= 0; j--) {
+			if (A[i] < A[j]) {
+				insert_point = j + 1;
+				break;
+			}
+			if (j == 0) {
+				insert_point = 0;
+			}
+		}
+		for (int j = i; j > insert_point; j--) {
+			A[j] = A[j - 1];
+		}
+		A[insert_point] = insert_value;
+	}
+	S[0] = A[0];
+
+	for (int i = 1; i < N; i++) {
+		S[i] = S[i - 1] + A[i];
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < N; i++) {
+		sum = sum + S[i];
+	}
+	cout << sum;
 }
